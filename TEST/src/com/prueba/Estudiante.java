2ef4293af0af8fb8ec43c0264e049023f9e2f840
Martineiko <palox.medina@gmail.com>
2017-10-17 09:21:31 -0500

    Clase estudiante

---
+++ TEST/src/com/prueba/Estudiante.java
@@ -0,0 +1,46 @@
+package com.prueba;
+
+public class Estudiante {
+	//Attributos de la clase, notese que son privados
+	private int codigo;
+	private String nombre;
+	//Constructor de la clase (por defecto)
+	public Estudiante()
+	{
+		
+	}
+	//Constructor sobrecargado
+	public Estudiante(int codigo, String nombre)
+	{
+		//Llamada a metodos
+		//Se establecen valores de forma segura
+		setCodigo(codigo);
+		setNombre(nombre);
+	}
+	//Como los atributos de la clase son privados, necesito una manera de acceder a sus valores
+	//Por eso se creo metodos para poder acceder de forma segura a estos
+	/**
+	 * @return the codigo
+	 */
+	public int getCodigo() {
+		return codigo;
+	}
+	/**
+	 * @param codigo the codigo to set
+	 */
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
